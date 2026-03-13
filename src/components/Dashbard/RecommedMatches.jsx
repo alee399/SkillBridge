45f610ff2ddb98573654be7262d905b745053918
Alee Sher <alesher2277@gmail.com>
2026-03-14 02:46:53 +0500

    changes

--- src/components/Dashbard/RecommedMatches.jsx
+++ src/components/Dashbard/RecommedMatches.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import MatchesSearchBar from "./MatchesSearchBar";
 import AiMatchScore from "../../common/ui/AiMatchScore";
-import frame from "../../assets/frame.png";
+import Frame from "../../assets/Frame.png";
 import Statslist from "./StatList";
 const RecommedMatches = () => {
   return (
     <div>
       <div className="flex gap-2 items-center my-8">
-        <img src={frame} alt="" className="w-7.5 h-7.5" />
+        <img src={Frame} alt="" className="w-7.5 h-7.5" />
         <h1 className="text-2xl font-semibold leading-[100%]">
           AI Recommended Matches
         </h1>
