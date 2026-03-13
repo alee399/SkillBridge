3c9b5fa0ed6b9c7e220c05f2ae87ccc0a3b4f974
Alee Sher <alesher2277@gmail.com>
2026-03-14 01:43:47 +0500

    changes

--- src/common/ui/LogoutBtn.jsx
+++ src/common/ui/LogoutBtn.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import {LogOutIcon} from 'lucide-react'
+import React from "react";
+import { LogOutIcon } from "lucide-react";
 
 const LogoutBtn = () => {
   return (
     <div>
-        <button className='absolute left-0 bottom-0 font-secondary text-xl flex gap-4 py-4 px-6 w-full items-center active:bg-menu-highlight text-text-gray active:text-primary cursor-pointer transition-colors duration-200'>
-            <LogOutIcon size={30} strokeWidth={1} />
-            <span>Log Out</span>
-        </button>
+      <button className="absolute left-0 bottom-0 font-secondary text-xl flex gap-4 py-4 px-6 w-full items-center active:bg-menu-highlight  active:text-primary cursor-pointer transition-colors duration-200 text-red-600">
+        <LogOutIcon size={30} strokeWidth={1} />
+        <span>Log Out</span>
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default LogoutBtn
+export default LogoutBtn;
--- src/pages/Dashboard/ChatPage.jsx
+++ src/pages/Dashboard/ChatPage.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import ChatSidebar from '../../components/Dashbard/Chat&Schedule/ChatSidebar'
-import ChatWindow from '../../components/Dashbard/Chat&Schedule/ChatWindow'
-import SessionScheduleSidebar from '../../components/Dashbard/Chat&Schedule/SessionScheduleSidebar'
-import PageWrapper from '../../components/Animation/PageWrapper'
+import React from "react";
+import ChatSidebar from "../../components/Dashbard/Chat&Schedule/ChatSidebar";
+import ChatWindow from "../../components/Dashbard/Chat&Schedule/ChatWindow";
+import SessionScheduleSidebar from "../../components/Dashbard/Chat&Schedule/SessionScheduleSidebar";
+import PageWrapper from "../../components/Animation/PageWrapper";
 
 const ChatPage = () => {
   return (
     <PageWrapper>
-    <div className='p-12'>
-      <div className='xl:flex w-full shadow-md rounded-[20px] border border-border-color overflow-hidden'>
-        <ChatSidebar />
-        <ChatWindow />
-        <SessionScheduleSidebar />  
+      <div className="p-12 ">
+        <div className="xl:flex w-full shadow-md rounded-[20px] border border-border-color overflow-hidden">
+          <ChatSidebar />
+          <ChatWindow />
+          <SessionScheduleSidebar />
+        </div>
       </div>
-    </div>
     </PageWrapper>
-  )
-}
+  );
+};
 
-export default ChatPage
+export default ChatPage;
